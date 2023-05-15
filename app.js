db04ecb0bdfe5e17caf16074d3d1ddb5fe7cf1f4
Jakob Feder <jako5372@hotmail.com>
2023-05-15 19:45:26 +0200

    Updated mqtt and added function space for db con

--- app.js
+++ app.js
@@ -2,9 +2,41 @@ const mqtt = require('mqtt')
 
 const mqttHandler = require("./mqtt_Handler")
 
-mqttHandler.mqttSubscribe("/test")
+const db = require("./dbHandler")
+
+mqttMessageOut = "hi"
+
+mqttHandler.mqttSubscribe("/00/IoTPlant/GDAToServer")
+
 
 mqttHandler.mqttListener()
+
+mqttMessageIn = mqttHandler.messageIn
+
+if (mqttMessageIn == 'rangeRequest') {
+    minLevel = db.dbCon('Select minLevel from Plants where ID = ', plantID, ';')
+    maxLevel = db.dbCon('Select maxLevel from Plants where ID = ', plantID, ';')
+
+    pubTopic = "/00/IoTPlant/ServerToGDA ", plantID
+    range = plantID, minLevel, maxLevel
+
+    mqttHandler.mqttPublish(pubTopic, range)
+
+}
+
+else if (mqttMessageIn == 'moistLevel') {
+    // function for posting moisturelevel to db
+}
+
+else if (mqttMessageIn == 'pumpFault') {
+    // function for posting pumpFault to db
+}
+mqttHandler.mqttPublish("/00/IoTPlant/ServerToGDA", mqttMessageOut)
+
+
+
+
+
 
 
 
--- publisher.js
+++
@@ -1,5 +0,0 @@
-const mqtt = require('mqtt')
-
-const mqttHandler = require("./pub_Handler")
-
-mqttHandler.mqttPublish("/test", "Hey U Ther?")
