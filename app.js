d23ffc31990dd4d476eab5e0dcdcb647a6fcc046
Jakob Feder <jako5372@hotmail.com>
2023-05-25 12:48:18 +0200

    Updated for mqtt message type

--- app.js
+++ app.js
@@ -4,34 +4,41 @@ const mqttHandler = require("./mqttHandler")
 
 const db = require("./dbHandler")
 
+const mqttSplit = '%'
+
 mqttHandler.mqttSubscribe("/00/IoTPlant/GDAToServer")
 
 mqttHandler.mqttListener()
 
-mqttMessageIn = JSON.parse(JSON.stringify(mqttHandler.messageIn))
+mqttMessageIn = mqttHandler.messageIn
 
-//Things needed to be recived in the MQTT from the GDA:
-// id (The CDA/ Plant id),
-// message (The command, that explains which 'if statement' the server needs to activate),
-// addWater (The amount of water calculated to be added) &
-// moistureLevel (The measured moisture level)
+data = mqttMessageIn.split(mqttSplit)
+
+// Things recived from the GDA is required to be in the following order:
+// message + '%' + id + '%' + moistureLevel + '%' + addWater
+
+// The different variables are descirbed further here:
+//  1. message = The command that explains which 'if statement' the server needs to activate
+//  2. id = The CDA or Plant id
+//  3. moistureLevel = The measured moisture level
+//  4. addWater = The amount of water calculated to be added
 
 
-plantID = mqttMessageIn[0].id
-
-if (mqttMessageIn[0].message == 'moistLevel') {
+if (data[0] == 'moistLevel') {
     // function for posting moisturelevel to db
+    plantID = data[1]
     date = new Date()
-    addedWater = mqttMessageIn[0].addWater
-    moistLevel = mqttMessageIn[0].moistureLevel
-    db.dbCon('Insert Into watering_record (added_water, plant_id, datetime, moisture) Values (' + addedWater + ', ' + plantID + ', ' + date + ', ' + moistLevel + ');')
+    moistLevel = data[2]
+    addedWater = data[3]
+    db.dbCon('INSERT INTO watering_record (added_water, plant_id, datetime, moisture) VALUES (' + addedWater + ', ' + plantID + ', ' + date + ', ' + moistLevel + ');')
     db.dbCon('Update Plant Set latest_m_level = ' + moistLevel + 'Where id = ' + plantID + ';')
     db.dbCon('Update Plant_information Set added_water_amount = ' + addedWater + 'Where id = ' + plantID +';')
 }
 
-else if (mqttMessageIn[0].message == 'pumpFault') {
+else if (data[0] == 'pumpFault') {
     // function for posting pumpFault to db
-    db.dbCon('Update Plant_information Set pump_fault = 1;')
+    plantID = data[1]
+    db.dbCon('UPDATE Plant_information WHERE id = ' + plantID + ' SET pump_fault = 1;')
 }
 else {
     console.log("unknow mqtt request")
--- mqttHandler.js
+++ mqttHandler.js
@@ -8,7 +8,6 @@ const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
 const connectUrl = `mqtt://${host}:${port}`
 
 subscribed = false
-messageIn = "mesIn Hi"
 
 const client = mqtt.connect(connectUrl, {
   clientId,
